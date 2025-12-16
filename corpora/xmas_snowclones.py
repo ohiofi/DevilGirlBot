6cc32d80a67304b70adfdb7bf02cb02dfe86ba2d
Justin Riley <20hz20khz@gmail.com>
2025-12-16 17:04:19 -0500

    Create xmas_snowclones.py

---
+++ corpora/xmas_snowclones.py
@@ -0,0 +1,85 @@
+XMAS_SNOWCLONES = [
+    # Twas the night before Christmas, when all through the house, Not a creature was stirring, not even a mouse
+    "Twas the *noun* before *noun*, when all through the house, Not *noun.a* was *verb.ing*, not *adverb* a mouse.",
+
+    # The stockings were hung by the chimney with care, In hopes that St. Nicholas soon would be there
+    "The *noun.s* were *verb.ed* by the *noun* with care, In *noun.s* that *name* *adverb* would be there.",
+
+    # The children were nestled all snug in their beds, While visions of sugar-plums danced in their heads
+    "The *noun.s* were *verb.ed* all *adjective* in their bed, While *noun.s* of *noun.s* *verb.ed* in their heads.",
+
+    # And mamma in her kerchief, and I in my cap, Had just settled our brains for a long winter's nap,
+    "And *noun* in her *noun*, and I in my cap, Had *adverb* *verb.ed* our *noun.s* for *adjective.a* *noun*'s nap.",
+    
+    # When out on the lawn there arose such a clatter, I sprang from my bed to see what was the matter.
+    "When *adverb* on the *noun* there *verb.ed* such a clatter, I *verb.ed* from my *noun* to *verb* what was the matter.",
+
+    # Away to the window I flew like a flash, Tore open the shutters and threw up the sash.
+    "*adverb* to the *noun* I *verb.ed* like a flash, *verb.ed* *adverb* the *noun.s* and *verb.ed* *adverb* the sash.",
+
+    # The moon on the breast of the new-fallen snow, Gave a lustre of midday to objects below,
+    "The *noun* on the *noun* of the *adjective* *adjective* snow, *verb.ed* *noun.a* of *noun* to *noun.s* below.",
+
+    # When what to my wondering eyes did appear, But a miniature sleigh and eight tiny rein-deer,
+    "When what to my *verb.ing* *noun.s* did appear, But *adjective.a* *noun* and *adjective* *adjective* reindeer.",
+
+    # With a little old driver so lively and quick, I knew in a moment she must be St. Nick.
+    "With *adjective.a* *adjective* *noun* so lively and quick, I *verb.ed* in *noun.a* it must be *name*.",
+
+    # More rapid than eagles her coursers they came, And she whertled, and shouted, and called them by name
+    "*comparativeadjective.title* *verb* than *noun.s* her *noun.s* they came, And she *verb.ed*, and *verb.ed*, and *verb.ed* them by name.",
+
+    # Now, Dasher! now, Dancer! now Prancer and Vixen! On, Comet! on, Cupid! on, Donder and Blitzen!
+    "Now, *name*! now, *name*! now *name* and *name*! On, *name*! on, *name*! on, *name* and *name*!",
+
+    # To the top of the porch! to the top of the wall! Now dash away! dash away! dash away all!
+    "To the *noun* of the *noun*! to the *noun* of the wall! *adverb.title* *verb* *adverb*! *verb* *adverb*! *verb* *adverb* all!",
+
+    # As leaves that before the wild hurricane fly, When they meet with an obstacle, mount to the sky
+    "As *noun.s* that *adverb* the *adjective* *noun* fly, When they *verb* with *noun.a*, *verb* to the sky.",
+
+    # So up to the housetop the coursers they flew, With the sleigh full of toys, and St. Nicholas too
+    "*adverb.title* *adverb* to the *noun* the *noun.s* they flew, With the *noun* *adverb* of *noun.s*, and *name* too.",
+
+    # And then, in a twinkling, I heard on the roof, The prancing and pawing of each little hoof
+    "And *adverb*, in *noun.a*, I *verb.ed* on the roof, The *verb.ing* and *verb.ing* of each *adjective* hoof.",
+
+    # As I drew in my head, and was turning around, Down the chimney St. Nicholas came with a bound
+    "As I *verb.ed* *adverb* my *noun*, and was *verb.ing* around, *adverb.title* the *noun* *name* *verb.ed* with a bound.",
+    
+    # She was dressed all in fur, from her head to her foot, And her clothes were all tarnished with ashes and soot
+    "She was *verb.ed* all in *noun*, from her *noun* to her foot, And her *noun.s* were all *adjective* with *noun.s* and soot.",
+
+    # A bundle of toys she had flung on her back, And she looked like a pedler just opening her pack
+    "*noun.a* of *noun.s* she had *verb.ed* on her back, And she *verb.ed* like *noun.a* *adverb* *verb.ing* her pack.",
+
+    # Her eyes—how they twinkled! Her dimples, how merry! Her cheeks were like roses, her nose like a cherry!
+    "Her *noun.s* how they *verb.ed*! Her *noun.s*, how merry! Her *noun.s* were like *noun.s*, her *noun* like a cherry!",
+
+    # Her droll little mouth was drawn up like a bow, And the beard on her chin was as white as the snow
+    "Her *adjective* *adjective* *noun* was *verb.ed* *adverb* like a bow, And the *noun* on her *noun* was *adverb* *adjective* as the snow.",
+
+    # The stump of a pipe she held tight in her teeth, And the smoke, it encircled her head like a wreath
+    "The *noun* of *noun.a* she *verb.ed* *adjective* in her teeth, And the *noun*, it *verb.ed* her *noun* like wreath.",
+
+    # She had a broad face and a little round belly, That shook when she laughed, like a bowl full of jelly
+    "She had *adjective.a* *noun* and *adjective.a* *adjective* belly, That *verb.ed* when she *verb.ed*, like *noun.a* *adverb* of jelly.",
+
+    # She was chubby and plump, a right jolly old elf, And I laughed when I saw him, in spite of myself
+    "She was *adjective* and *adjective*, *adjective.a* *adjective* *adjective* elf, And I *verb.ed* when I *verb.ed* her, in spite of myself.",
+
+    # A wink of her eye and a twist of her head, Soon gave me to know I had nothing to dread
+    "*noun.a* of her *noun* and *noun.a* of her head, *adverb.title* *verb.ed* me to *verb* I had *noun* to dread.",
+
+    # She spoke not a word, but went straight to her work, And filled all the stockings; then turned with a jerk
+    "She *verb.ed* *adverb* *noun.a*, but *verb.ed* *adverb* to her work, And *verb.ed* all the *noun.s*; *adverb* *verb.ed* with a jerk.",
+    
+    # And laying her finger aside of her nose, And giving a nod, up the chimney she rose
+    "And *verb.ing* her *noun* *adverb* of her nose, And *verb.ing* *noun.a*, *adverb* the *noun* she rose.",
+
+    # She sprang to her sleigh, to her team gave a whertle, And away they all flew like the down of a thertle
+    "She *verb.ed* to her *noun*, to her *noun* *verb.ed* whistle, And *adverb* they all *verb.ed* like the *noun* of a thistle.",
+
+    # But I heard him exclaim, ere she drove out of sight, “Happy Christmas to all, and to all a good night!”
+    "But I *verb.ed* her *verb*, *adverb* she *verb.ed* *adverb* of sight, “*adjective.title* *noun* to all, and to all *adjective.a* night!”"
+]
