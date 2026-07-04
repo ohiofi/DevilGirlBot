85194c143ee5d57fae20fd0801d5289fdba87b1c
Justin Riley <20hz20khz@gmail.com>
2026-07-04 17:20:53 -0400

    Update scrape_toots_to_pool.py

--- scrape_toots_to_pool.py
+++ scrape_toots_to_pool.py
@@ -8,7 +8,8 @@ import os, re, json, time
 
 load_dotenv()
 mastodon = Mastodon(access_token=os.getenv("access_token"), api_base_url="https://mastodon.social")
-BANLIST = json.loads(os.getenv("banlist"))
+BANNED_WORDS = json.loads(os.getenv("banned_words"))
+BANNED_USERS = json.loads(os.getenv("banned_users"))
 
 def scrape_monsterdon():
     sentence_pool = load_sentences()
@@ -44,6 +45,7 @@ def scrape_monsterdon():
 
             for toot in toots:
                 source_url = toot["url"]
+                author = toot["account"]["acct"]
                 full_text = text_only_cleaning_algorithm(toot["content"])
                 full_text = replace_non_terminating_punctuation(full_text)
 
@@ -52,7 +54,7 @@ def scrape_monsterdon():
 
                 # 1 toot can have many sentences. Each sentence is added to the sentence pool separately
                 for s in new_sentences:
-                    if is_sentence_valid(s, sentence_pool, history_text_only, BANLIST):
+                    if is_sentence_valid(full_text, author, sentence_pool, history_text_only, BANNED_WORDS, BANNED_USERS):
                         sentence_pool.append({"sentence": s, "url": source_url})
                         new_toots_count += 1
 
